7ef4df58dd355ee9e52e4de6dc6fab3ba002542a
Boseung01 <45059085+BoseungJeong@users.noreply.github.com>
2026-04-04 14:13:07 +0900

    Update index.tsx

--- src/component/invitation/index.tsx
+++ src/component/invitation/index.tsx
@@ -25,15 +25,12 @@ export const Invitation = () => {
 
       <div className="break" />
 
-      <div className="content">초록이 가득한 8월 여름의 한복판에서,</div>
-      <div className="break" />
-      <div className="content">저희 두 사람이 서로의 계절이 되어</div>
+      <div className="content">초록이 깊어가는 8월의 어느 날,</div>
+      <div className="content">서로에게 가장 따뜻한 계절이 되어 줄 두 사람이</div>
       <div className="content">새로운 이야기를 시작하려 합니다.</div>
-      <div className="break" />
-      <div className="content">싱그러운 여름의 첫 장처럼</div>
-      <div className="content">맑고 선명한 마음으로 잘 살아가겠습니다.</div>
-      <div className="break" />
-      <div className="content">그 시작의 자리에 늘 곁에서 아껴주셨던</div>
+      <div className="content">푸르른 날의 약속처럼</div>
+      <div className="content">맑고 진실한 마음으로 살아가겠습니다.</div>
+      <div className="content">그 시작의 자리에 늘 곁에서 아껴주신</div>
       <div className="content">소중한 분들을 모십니다.</div>
 
       <div className="break" />
